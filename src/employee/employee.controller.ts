21a7404795e6d578a4ebb7703c317fff791c22c3
Ltensio <76816528+Ltensio@users.noreply.github.com>
2024-12-08 15:35:14 +0100

    feat: added controller and service methods for crud operations on employee entity

--- src/employee/employee.controller.ts
+++ src/employee/employee.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { EmployeeService } from './employee.service';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
-import { UpdateEmployeeDto } from './dto/update-employee.dto';
+import { UpdateContactInfoDto, UpdateEmployeeDto, UpdateManagerDto } from './dto/update-employee.dto';
+import { EmployeeQueryDto } from './dto/employee-query.dto';
+import { UuidValidationPipe } from 'src/pipes/uuid.validation.pipe';
 
 @Controller('employee')
 export class EmployeeController {
@@ -13,22 +15,32 @@ export class EmployeeController {
   }
 
   @Get()
-  findAll() {
-    return this.employeeService.findAll();
+  findAll(@Query() findEmployeeDto: EmployeeQueryDto) {
+    return this.employeeService.findAll(findEmployeeDto);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.employeeService.findOne(+id);
+  findEmployee(@Param('id', UuidValidationPipe) id: string) {
+    return this.employeeService.findEmployee(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateEmployeeDto: UpdateEmployeeDto) {
-    return this.employeeService.update(+id, updateEmployeeDto);
+  updateEmployee(@Param('id', UuidValidationPipe) employeeId: string, @Body() updateEmployeeDto: UpdateEmployeeDto) {
+    return this.employeeService.updateEmployee(employeeId, updateEmployeeDto);
+  }
+
+  @Patch('updatemanager/:id')
+  addManager(@Param('id', UuidValidationPipe) employeeId: string, @Body() updateManagerDto: UpdateManagerDto) {
+    return this.employeeService.addManager(employeeId, updateManagerDto);
+  }
+
+  @Patch('updatecontactinfo/:id')
+  updateContactInfo(@Param('id', UuidValidationPipe) employeeId: string, @Body() updateContactInfoDto: UpdateContactInfoDto) {
+    return this.employeeService.updateContactInfo(employeeId, updateContactInfoDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.employeeService.remove(+id);
+  remove(@Param('id', UuidValidationPipe) id: string) {
+    return this.employeeService.remove(id);
   }
 }
