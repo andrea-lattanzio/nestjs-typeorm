ed8e0808d4a0a2dcbb1aa28b664a3ee1c92cd649
Ltensio <76816528+Ltensio@users.noreply.github.com>
2024-12-08 16:11:03 +0100

    feat: add file and folder structure for meeting entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { DatabaseModule } from './database/database.module';
 import { EmployeeModule } from './employee/employee.module';
 import { ConfigModule } from '@nestjs/config';
 import { TaskModule } from './task/task.module';
+import { MeetingModule } from './meeting/meeting.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, EmployeeModule, TaskModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, EmployeeModule, TaskModule, MeetingModule],
   controllers: [AppController],
   providers: [AppService],
 })
