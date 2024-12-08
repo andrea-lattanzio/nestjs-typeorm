8514afcfa7d933542468c06989bcffdd56c95cf2
Ltensio <76816528+Ltensio@users.noreply.github.com>
2024-12-08 16:17:11 +0100

    feat: add service and controller methods for meeting crud operations

--- src/meeting/meeting.controller.ts
+++ src/meeting/meeting.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { MeetingService } from './meeting.service';
 import { CreateMeetingDto } from './dto/create-meeting.dto';
-import { UpdateMeetingDto } from './dto/update-meeting.dto';
+import { UpdateAttendeesDto, UpdateMeetingDto } from './dto/update-meeting.dto';
+import { findMeetingDto } from './dto/find-meeting.dto';
+import { UuidValidationPipe } from 'src/pipes/uuid.validation.pipe';
 
 @Controller('meeting')
 export class MeetingController {
@@ -18,17 +20,27 @@ export class MeetingController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.meetingService.findOne(+id);
+  findOne(@Param('id', UuidValidationPipe) id: string) {
+    return this.meetingService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateMeetingDto: UpdateMeetingDto) {
-    return this.meetingService.update(+id, updateMeetingDto);
+  update(@Param('id', UuidValidationPipe) id: string, @Body() updateMeetingDto: UpdateMeetingDto) {
+    return this.meetingService.update(id, updateMeetingDto);
+  }
+
+  @Patch('addattendees/:id')
+  addAttendees(@Param('id', UuidValidationPipe) id: string, @Body() addAttendeesDto: UpdateAttendeesDto) {
+    return this.meetingService.addAttendees(id, addAttendeesDto);
+  }
+
+  @Patch('removeattendees/:id')
+  removeAttendees(@Param('id', UuidValidationPipe) id: string, @Body() removeAttendeesDto: UpdateAttendeesDto) {
+    return this.meetingService.removeAttendee(id, removeAttendeesDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.meetingService.remove(+id);
+  remove(@Param('id', UuidValidationPipe) id: string) {
+    return this.meetingService.remove(id);
   }
 }
