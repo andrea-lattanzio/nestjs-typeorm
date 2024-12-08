96b0cb64c9908c0837fbdea5d7f72e0ac2ca53fc
Ltensio <76816528+Ltensio@users.noreply.github.com>
2024-12-08 15:43:53 +0100

    feat: added global validation pipe and morgan logger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('/api');
+  app.use(morgan('dev'));
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true
+    }),
+  );
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get<number>('PORT'));
 }
 bootstrap();
